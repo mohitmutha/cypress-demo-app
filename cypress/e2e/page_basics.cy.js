97367075f3381962fb5e83f598826b7e169d10a5
Mohit <mutha.mohit@siemens.com>
2023-02-05 12:57:45 +0530

    Add fixtures

--- cypress/e2e/page_basics.cy.js
+++ cypress/e2e/page_basics.cy.js
@@ -1,14 +1,19 @@
 describe('basic page check', () => {
   it('header text', () => {
     cy.visit('http://localhost:3001')
-    cy.get('[data-testid=page-title]').should('have.text','Calculator')
+    cy.fixture('elementLocators').then(elementLocators => {
+      cy.get(elementLocators.pageTitle).should('have.text', 'Calculator')
+    })
+
   })
 
   it('input 5', () => {
     cy.visit('http://localhost:3001')
-    cy.get('#input-value').type(5)
-    cy.get('#square-result-value').should('have.text',25)
-    cy.get('#cube-result-value').should('have.text',125)
-    cy.get('#fibonacci-result-value').should('have.text',3)
+    cy.fixture('elementLocators').then(elementLocators => {
+      cy.get(elementLocators.numberInput).type(5)
+      cy.get(elementLocators.squareResult).should('have.text', 25)
+      cy.get(elementLocators.cubeResult).should('have.text', 125)
+      cy.get(elementLocators.fibonacciResult).should('have.text', 3)
+    })
   })
 })
